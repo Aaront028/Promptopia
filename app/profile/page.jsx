8410b4db76e2cb4cc0ac25ffcdf797d9720d6b3a
Aaron T <info@aarontan.co.nz>
2024-07-18 14:17:19 +1200

    WIP - Full CRUD operation working on all endpoints

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -6,6 +6,8 @@ import { useRouter } from 'next/navigation'
 import Profile from '@components/Profile'
 
 const MyProfile = () => {
+  const router = useRouter()
+
   const { data: session } = useSession()
   const [posts, setPosts] = useState([])
 
@@ -16,11 +18,31 @@ const MyProfile = () => {
       setPosts(data)
     }
     if (session?.user.id) fetchPosts()
-  }, [])
+  }, [session?.user.id])
 
-  const handleEdit = () => {}
+  const handleEdit = (post) => {
+    router.push(`/update-prompt?id=${post._id}`)
+  }
 
-  const handleDelete = async () => {}
+  const handleDelete = async (post) => {
+    const hasConfirmed = confirm('Are you sure you want to delete this prompt?')
+
+    if (hasConfirmed) {
+      try {
+        await fetch(`/api/prompt/${post._id.toString()}`, {
+          method: 'DELETE',
+        })
+
+        const filteredPosts = posts.filter((item) => item._id !== post._id)
+
+        setPosts(filteredPosts)
+
+        router.push('/')
+      } catch (error) {
+        console.log(error)
+      }
+    }
+  }
 
   return (
     <Profile
@@ -28,7 +50,7 @@ const MyProfile = () => {
       desc="Welcome to your personalized profile page"
       data={posts}
       handleEdit={handleEdit}
-      HandleDelete={handleDelete}
+      handleDelete={handleDelete}
     />
   )
 }
